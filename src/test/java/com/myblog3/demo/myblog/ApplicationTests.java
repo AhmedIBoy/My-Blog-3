513dc66a83065f9f1b10458ab04ed32b0ac4cc5f
ahmednandoliya <Iboy85000@gmail.com>
2024-01-04 12:10:22 +0530

    128 : Post Feature Developing face

---
+++ src/test/java/com/myblog3/demo/myblog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog3.demo.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
